f828f58492f0b5497088bc0c1a5c1b7ebe84287a
JulieBodette <jr3python@gmail.com>
2022-09-15 00:47:18 -0400

    IT WORKS
    Data is written to the Sqlite database and to the data table in the Wisej application

--- WisejWebApplication1/WisejWebApplication1/Window1.cs
+++ WisejWebApplication1/WisejWebApplication1/Window1.cs
@@ -14,43 +14,19 @@ namespace WisejWebApplication1
             InitializeComponent();
         }
         BindingList<Organization> OrganizationList = new BindingList<Organization>();
+
+        //create a new sqlite database. We defined the database class in Database.cs, it just makes a SQLite Connection
+        Database databaseObject = new Database();
+
+        //NOTE: databaseObject.myConnection is just a SQLiteConnection we created in the constructor of the databaseObject
+        //you could do this without creating a databaseObject- just create the SQLiteConnection myConnection
+        //OpenConnection() and CloseCOnnection() are also functions we created in the databaseObject
+
+
         private void Window1_Load(object sender, EventArgs e)
         {
             OrganizationList.Add(new Organization() { Name = "examplecompany", City = "Cleveland", Street = "101 Example Ave", Country = "USA", Zip = 44142 });
             dataGridView1.DataSource = OrganizationList;//connect the data list to the grid on the UI
-
-            //create a new sqlite database. We defined the database class in Database.cs, it just makes a SQLite Connection
-            Database databaseObject = new Database();
-
-            //NOTE: myConnection is just a SQLiteConnection we created in the constructor of the databaseObject
-            //you could do this without creating a databaseObject- just create the SQLiteConnection myConnection
-            //OpenConnection() and CloseCOnnection() are also functions we created in the databaseObject
-
-
-            
-
-            //SQL query to insert data into the database
-            string query = "INSERT INTO Organizations ('Name', 'Street', 'Zip', 'City', 'Country') VALUES (@Name, @Street, @Zip, @City, @Country)";
-            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection); //send it the query and the SQLite connection
-
-            //open the connection to the database
-            databaseObject.OpenConnection();
-
-
-            //Here we tell the query what values to send in
-            myCommand.Parameters.AddWithValue("@Name", "Company Name3 Here");
-            myCommand.Parameters.AddWithValue("@Street", "Street Name2 Here");
-            myCommand.Parameters.AddWithValue("@Zip", 33);
-            myCommand.Parameters.AddWithValue("@City", "City Name Here2");
-            myCommand.Parameters.AddWithValue("@Country", "Country Name Here2");
-
-            //execute the query
-           // myCommand.ExecuteNonQuery(); //this returns an integer- the number of rows affected in the database
-            //you could store it in a variable if you wanted to
-
-
-            //close the connection to the database
-            databaseObject.CloseConnection();
 
         }
 
@@ -64,8 +40,31 @@ namespace WisejWebApplication1
                 OrganizationList.Add(new Organization() { Name = typedTextBox1.Text, Street = typedTextBox2.Text, Zip = zipcode, City = typedTextBox4.Text, Country = typedTextBox5.Text });
                 AlertBox.Show("You added an organization to the database");
 
-                //add code to also write the data to a sql database
-                //backup plan in case i cannot writethe OrganizationList directly
+                //write the data to a sql database
+
+                //SQL query to insert data into the database
+                string query = "INSERT INTO Organizations ('Name', 'Street', 'Zip', 'City', 'Country') VALUES (@Name, @Street, @Zip, @City, @Country)";
+                SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection); //send it the query and the SQLite connection
+
+                //open the connection to the database
+                databaseObject.OpenConnection();
+
+                //Here we tell the query what values to send in
+                myCommand.Parameters.AddWithValue("@Name", typedTextBox1.Text);
+                myCommand.Parameters.AddWithValue("@Street", typedTextBox2.Text);
+                myCommand.Parameters.AddWithValue("@Zip", zipcode);
+                myCommand.Parameters.AddWithValue("@City", typedTextBox4.Text);
+                myCommand.Parameters.AddWithValue("@Country", typedTextBox5.Text);
+
+                //execute the query
+                myCommand.ExecuteNonQuery(); //this returns an integer- the number of rows affected in the database
+                                             //you could store it in a variable if you wanted to
+
+                //close the connection to the database
+                databaseObject.CloseConnection();
+
+
+
             }
             catch(System.FormatException) //show alert if zip code is invalid
             {
