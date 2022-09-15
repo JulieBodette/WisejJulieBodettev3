fed078ff2c81711fa70aacd7efa06a6fbf0d130c
JulieBodette <jr3python@gmail.com>
2022-09-15 01:22:23 -0400

    Made it look pretty
    moved stuff around so that it's centered- added something to tell the user to enter an organization

--- WisejWebApplication1/WisejWebApplication1/Window1.Designer.cs
+++ WisejWebApplication1/WisejWebApplication1/Window1.Designer.cs
@@ -29,18 +29,19 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new Wisej.Web.TableLayoutPanel();
+            this.typedTextBox5 = new Wisej.Web.TypedTextBox();
+            this.typedTextBox4 = new Wisej.Web.TypedTextBox();
+            this.typedTextBox3 = new Wisej.Web.TypedTextBox();
+            this.typedTextBox2 = new Wisej.Web.TypedTextBox();
+            this.typedTextBox1 = new Wisej.Web.TypedTextBox();
             this.label5 = new Wisej.Web.Label();
             this.label4 = new Wisej.Web.Label();
             this.label3 = new Wisej.Web.Label();
             this.label2 = new Wisej.Web.Label();
             this.label1 = new Wisej.Web.Label();
             this.dataGridView1 = new Wisej.Web.DataGridView();
-            this.typedTextBox1 = new Wisej.Web.TypedTextBox();
-            this.typedTextBox2 = new Wisej.Web.TypedTextBox();
-            this.typedTextBox3 = new Wisej.Web.TypedTextBox();
-            this.typedTextBox4 = new Wisej.Web.TypedTextBox();
-            this.typedTextBox5 = new Wisej.Web.TypedTextBox();
             this.button1 = new Wisej.Web.Button();
+            this.label6 = new Wisej.Web.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -50,17 +51,17 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 13.47F));
             this.tableLayoutPanel1.ColumnStyles.Add(new Wisej.Web.ColumnStyle(Wisej.Web.SizeType.Percent, 86.53F));
-            this.tableLayoutPanel1.Controls.Add(this.typedTextBox5, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.typedTextBox4, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.typedTextBox3, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.typedTextBox2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.typedTextBox1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(80, 29);
+            this.tableLayoutPanel1.Controls.Add(this.typedTextBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.typedTextBox5, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(200, 29);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Absolute, 25F));
@@ -70,6 +71,56 @@
             this.tableLayoutPanel1.RowStyles.Add(new Wisej.Web.RowStyle(Wisej.Web.SizeType.Absolute, 25F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(408, 131);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // typedTextBox5
+            // 
+            this.typedTextBox5.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+            this.typedTextBox5.Location = new System.Drawing.Point(57, 103);
+            this.typedTextBox5.Name = "typedTextBox5";
+            this.typedTextBox5.Size = new System.Drawing.Size(348, 22);
+            this.typedTextBox5.TabIndex = 9;
+            this.typedTextBox5.ValueType = typeof(string);
+            // 
+            // typedTextBox4
+            // 
+            this.typedTextBox4.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+            this.typedTextBox4.Location = new System.Drawing.Point(57, 78);
+            this.typedTextBox4.Name = "typedTextBox4";
+            this.typedTextBox4.Size = new System.Drawing.Size(348, 19);
+            this.typedTextBox4.TabIndex = 8;
+            this.typedTextBox4.ValueType = typeof(string);
+            // 
+            // typedTextBox3
+            // 
+            this.typedTextBox3.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+            this.typedTextBox3.Location = new System.Drawing.Point(57, 53);
+            this.typedTextBox3.Name = "typedTextBox3";
+            this.typedTextBox3.Size = new System.Drawing.Size(348, 19);
+            this.typedTextBox3.TabIndex = 7;
+            this.typedTextBox3.ValueType = typeof(string);
+            // 
+            // typedTextBox2
+            // 
+            this.typedTextBox2.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+            this.typedTextBox2.Location = new System.Drawing.Point(57, 28);
+            this.typedTextBox2.Name = "typedTextBox2";
+            this.typedTextBox2.Size = new System.Drawing.Size(348, 19);
+            this.typedTextBox2.TabIndex = 6;
+            this.typedTextBox2.ValueType = typeof(string);
+            // 
+            // typedTextBox1
+            // 
+            this.typedTextBox1.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
+            | Wisej.Web.AnchorStyles.Right)));
+            this.typedTextBox1.Location = new System.Drawing.Point(57, 3);
+            this.typedTextBox1.Name = "typedTextBox1";
+            this.typedTextBox1.Size = new System.Drawing.Size(348, 19);
+            this.typedTextBox1.TabIndex = 5;
+            this.typedTextBox1.ValueType = typeof(string);
             // 
             // label5
             // 
@@ -118,76 +169,37 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.Anchor = Wisej.Web.AnchorStyles.Top;
-            this.dataGridView1.Location = new System.Drawing.Point(83, 207);
+            this.dataGridView1.Anchor = ((Wisej.Web.AnchorStyles)((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Right)));
+            this.dataGridView1.Location = new System.Drawing.Point(3, 225);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(566, 171);
+            this.dataGridView1.Size = new System.Drawing.Size(770, 204);
             this.dataGridView1.TabIndex = 3;
-            // 
-            // typedTextBox1
-            // 
-            this.typedTextBox1.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
-            | Wisej.Web.AnchorStyles.Right)));
-            this.typedTextBox1.Location = new System.Drawing.Point(57, 3);
-            this.typedTextBox1.Name = "typedTextBox1";
-            this.typedTextBox1.Size = new System.Drawing.Size(348, 19);
-            this.typedTextBox1.TabIndex = 5;
-            this.typedTextBox1.ValueType = typeof(string);
-            // 
-            // typedTextBox2
-            // 
-            this.typedTextBox2.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
-            | Wisej.Web.AnchorStyles.Right)));
-            this.typedTextBox2.Location = new System.Drawing.Point(57, 28);
-            this.typedTextBox2.Name = "typedTextBox2";
-            this.typedTextBox2.Size = new System.Drawing.Size(348, 19);
-            this.typedTextBox2.TabIndex = 6;
-            this.typedTextBox2.ValueType = typeof(string);
-            // 
-            // typedTextBox3
-            // 
-            this.typedTextBox3.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
-            | Wisej.Web.AnchorStyles.Right)));
-            this.typedTextBox3.Location = new System.Drawing.Point(57, 53);
-            this.typedTextBox3.Name = "typedTextBox3";
-            this.typedTextBox3.Size = new System.Drawing.Size(348, 19);
-            this.typedTextBox3.TabIndex = 7;
-            this.typedTextBox3.ValueType = typeof(string);
-            // 
-            // typedTextBox4
-            // 
-            this.typedTextBox4.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
-            | Wisej.Web.AnchorStyles.Right)));
-            this.typedTextBox4.Location = new System.Drawing.Point(57, 78);
-            this.typedTextBox4.Name = "typedTextBox4";
-            this.typedTextBox4.Size = new System.Drawing.Size(348, 19);
-            this.typedTextBox4.TabIndex = 8;
-            this.typedTextBox4.ValueType = typeof(string);
-            // 
-            // typedTextBox5
-            // 
-            this.typedTextBox5.Anchor = ((Wisej.Web.AnchorStyles)(((Wisej.Web.AnchorStyles.Top | Wisej.Web.AnchorStyles.Left) 
-            | Wisej.Web.AnchorStyles.Right)));
-            this.typedTextBox5.Location = new System.Drawing.Point(57, 103);
-            this.typedTextBox5.Name = "typedTextBox5";
-            this.typedTextBox5.Size = new System.Drawing.Size(348, 22);
-            this.typedTextBox5.TabIndex = 9;
-            this.typedTextBox5.ValueType = typeof(string);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(535, 98);
+            this.button1.Location = new System.Drawing.Point(333, 174);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 27);
             this.button1.TabIndex = 4;
             this.button1.Text = "Add to Database";
             this.button1.MouseClick += new Wisej.Web.MouseEventHandler(this.button1_MouseClick);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ImageSource = "(none)";
+            this.label6.Location = new System.Drawing.Point(242, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(309, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Please Enter an Organization to add to the Database";
+            // 
             // Window1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(776, 432);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -198,6 +210,7 @@
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -216,6 +229,7 @@
         private Wisej.Web.TypedTextBox typedTextBox2;
         private Wisej.Web.TypedTextBox typedTextBox1;
         private Wisej.Web.Button button1;
+        private Wisej.Web.Label label6;
     }
 }
 
