d288d9d1488cfc9501e10c25d79618a4c46444d9
JulieBodette <jr3python@gmail.com>
2022-09-14 23:26:59 -0400

    creates sqlite database on starup
    database is only created if it does not already exist

---
+++ WisejWebApplication1/WisejWebApplication1/Database.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SQLite; //import SQlite so that we can use it
+using System.IO; //so we can check if the database file already exists
+using Wisej.Web;
+
+namespace WisejWebApplication1
+{
+    public class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()//constructor
+        {
+
+            myConnection = new SQLiteConnection("Data Source=database.sqlite3"); //constructor accepts a string
+
+            //only create a new database if one does not already exist
+            if (!File.Exists("./database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3"); //creates the database
+                AlertBox.Show("Database created");
+            }
+
+        }
+    }
+}
--- WisejWebApplication1/WisejWebApplication1/Window1.cs
+++ WisejWebApplication1/WisejWebApplication1/Window1.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.ComponentModel;
+using System.Data.Entity;
 using Wisej.Web;
 
 namespace WisejWebApplication1
@@ -16,16 +17,23 @@ namespace WisejWebApplication1
         {
             OrganizationList.Add(new Organization() { Name = "examplecompany", City = "Cleveland", Street = "101 Example Ave", Country = "USA", Zip = 44142 });
             dataGridView1.DataSource = OrganizationList;//connect the data list to the grid on the UI
+
+            //create a new sqlite database
+            Database databaseObject = new Database();
         }
 
         private void button1_MouseClick(object sender, MouseEventArgs e)
         {
             try
             {
+                //this converts the text in the Zip box to an integer
                 OrganizationList.Add(new Organization() { Name = typedTextBox1.Text, Street = typedTextBox2.Text, Zip = int.Parse(typedTextBox3.Text), City = typedTextBox4.Text, Country = typedTextBox5.Text });
-                AlertBox.Show("You added something to the database");
+                AlertBox.Show("You added an organization to the database");
+
+                //add code to also write the data to a sql database
+                //backup plan in case i cannot writethe OrganizationList directly
             }
-            catch
+            catch //show alert if zip code is invalid
             {
                 AlertBox.Show("Please enter a number for the zip code");
             }
